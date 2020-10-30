72e861b6b84c5a0bd738ee1783d0a37e9adee698
unknown <1273163614@qq.com>
2020-10-30 16:48:44 +0800

    个人中心页开发

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,50 +1,57 @@
 <template>
 	<view>
-      <button type="default" open-type="getAuthorize" @getAuthorize="getPhoneNumber" @error="onAuthError" scope='phoneNumber'>授权手机号</button>
+		<view class="top flex align-center justify-center"></view>
+		<view class="flex align-center">
+			<view class="flex align-center justify-center position-relative" style="width: 180rpx;height: 160rpx;">
+				<image
+					src="../../static/1.jpeg"
+					class="rounded-circle"
+					style="width: 145rpx;height: 145rpx;position: absolute;top: -60rpx;"
+				></image>
+			</view>
+			<view class="flex flex-column">
+				<text class="font-md">小吴</text>
+				<text class="font text-muted">最月半的</text>
+			</view>
+			<view class="ml-auto mr-3">
+				<view class="border border-main rounded flex align-center justify-center p-2" hover-class="bg-light">
+					<text class="text-main font">编辑资料</text>
+				</view>
+			</view>
+		</view>
+		<view class="f-divider"></view>
+		<f-list-item icon="iconbizhongguanli" title="我的金币" :showRight="false">
+			<text class="text-muted font">50金币 立即充值</text>
+		</f-list-item>
+		<f-list-item icon="iconzhengzaizhibo" title="我的直播"><text class="text-muted font">0</text></f-list-item>
+		<f-list-item icon="iconfaxian" title="我的关注"><text class="text-muted font">0</text></f-list-item>
+		<f-list-item icon="iconmore" title="历史记录"></f-list-item>
 	</view>
 </template>
 <script>
+import fListItem from '@/components/live/f-list-item.vue';
 export default {
-  data() {
-    return {};
-  },
-  methods: {
-    getPhoneNumber() {
-      this.onGetAuthorize()
-        .then(res => {
-          // console.log(res)
-          var resData = JSON.parse(res.response);
-          // console.log(resData);
-          // 重新赋值方便后台获取
-          var params = {
-            phoneNumber: resData.response,
-            sign: resData.sign
-          };
-          console.log(params);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-    onGetAuthorize() {
-      return new Promise((resolve, reject) => {
-        my.getPhoneNumber({
-          scopes: "auth_user",
-          success: res => {
-            resolve(res);
-          },
-          fail: res => {
-            reject(res);
-          }
-        });
-      });
-    },
-    onAuthError() {
-      console.log("123321");
-    }
-  }
+	components: {
+		fListItem
+	},
+	data() {
+		return {
+			statusBarHeight: 0
+		};
+	},
+	onLoad() {
+		let res = uni.getSystemInfoSync();
+		this.statusBarHeight = res.statusBarHeight;
+	},
+	methods: {}
 };
 </script>
-<style>
-
+<style scoped>
+.top {
+	width: 750rpx;
+	height: 260rpx;
+	background-image: url(../../static/4.jpeg);
+	background-size: cover;
+	background-image: linear-gradient(to right, #ba7ace 0%, #8745ff 100%);
+}
 </style>
